f7dea40e1e0a2c4960174d085c627b2e8f5b5b00
Gyeongmun-Yu <dev.gmyu@gmail.com>
2023-08-28 16:34:00 +0900

    chore : Initialize server directory

---
+++ Server/main07/src/main/java/com/codestates/main07/Main07Application.java
@@ -0,0 +1,13 @@
+package com.codestates.main07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main07Application.class, args);
+	}
+
+}
