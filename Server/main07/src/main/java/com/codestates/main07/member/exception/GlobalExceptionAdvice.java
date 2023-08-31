54468395dbae665302b11f1403c3679f764e6de9
park <rjf654@gmail.com>
2023-08-31 18:05:42 +0900

    feat : Exception패키지 -> ErrorResponse/GlobalExceptionAdvice 클래스 구현

---
+++ Server/main07/src/main/java/com/codestates/main07/member/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,31 @@
+package com.codestates.main07.member.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+
+
+@RestControllerAdvice // Controller 클래스에서 발생하는 예외를 도맡아서 처리
+public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
+    }
+}
