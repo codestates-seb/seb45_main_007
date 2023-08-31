29d94ec328ccd079608d988dc47ab8a64bf6206d
Gyeongmun-Yu <dev.gmyu@gmail.com>
2023-08-31 20:56:30 +0900

    refactor : Delete search pakage

--- Server/main07/src/main/java/com/codestates/main07/search/search/SearchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.main07.search.search;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SearchRepository extends JpaRepository<Search, Long> {
-}
