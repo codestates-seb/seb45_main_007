cc55ac7a17d76fdb933c354e746aa5736ede30d5
tjddnjs0102 <rjf654@gmail.com>
2023-09-01 18:17:40 +0900

    feat : 메인 클래스와 멤버 엔티티 수정

--- Server/main07/src/main/java/com/codestates/main07/member/entity/Member.java
+++ Server/main07/src/main/java/com/codestates/main07/member/entity/Member.java
@@ -4,16 +4,53 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
 
 
 @Getter
 @Setter
 @NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동 생성
 @AllArgsConstructor // 멤버 클래스의 모든 멤버 변수를 파라미터로 갖는 멤버 생성자를 자동 생성
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long memberId;
+
+    @Column(unique = true, length = 45)
+    @Email(message = "이메일 형식이 아닙니다.")
     private String email;
+
+    @Column(length = 16)
+    @Size(min = 4, max = 16, message = "4자 이상 16자 이하로 입력해주세요.")
+    @Pattern(regexp = "^[가-힣a-zA-Z]*$", message = "한글 및 영어만 가능합니다.")
     private String username;
+
+    @Column(length = 16)
+    @Size(min = 4, max = 16, message = "4자 이상 16자 이하로 입력해주세요.")
+    @Pattern(regexp = "^[가-힣a-zA-Z]*$", message = "한글 및 영어만 가능합니다.")
     private String nickname;
+
+    @Column(length = 16)
+    @Size(min = 8, max = 16, message = "8자 이상 16자 이하로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W).*$", message = "영문, 숫자, 특수문자를 포함해주세요.")
     private String password;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 }
