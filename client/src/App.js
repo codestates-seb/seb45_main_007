320a84d76d0d8e817d1884f99b7839fb7102b098
LEEHYUNGSU <gudtn0506@gmail.com>
2023-09-08 08:19:56 +0900

    feat: Board Page Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { SchoolTheme } from "./pages/SchoolTheme.jsx";
+import { Board } from "./pages/Board.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SchoolTheme />} />
+        <Route path="/SchoolHome" element={<SchoolTheme />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
