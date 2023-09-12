2f7f57ef85119ed3d4b0bc67cd41ec50491e1bd6
JCI <claeo3@gmail.com>
2023-09-12 16:37:20 +0900

    feat : 빌드 에러 해결

--- Server/main07/src/main/java/com/codestates/main07/clubBoardRecommend/mapper/RecommendMapper.java
+++ Server/main07/src/main/java/com/codestates/main07/clubBoardRecommend/mapper/RecommendMapper.java
@@ -4,17 +4,19 @@ import com.codestates.main07.clubBoardRecommend.dto.RecommendPatchDto;
 import com.codestates.main07.clubBoardRecommend.dto.RecommendPostDto;
 import com.codestates.main07.clubBoardRecommend.dto.RecommendResponseDto;
 import com.codestates.main07.clubBoardRecommend.entity.Recommend;
+import com.codestates.main07.clubBoard.board.entity.ClubBoard;
+
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RecommendMapper {
-    @Mapping(source = "recommended", target = "recommended2")
+    @Mapping(source = "clubBoard.clubBoardId", target = "clubBoardId")
     Recommend recommendPostDtoToRecommend(RecommendPostDto recommendPostDto);
-    @Mapping(source = "recommended", target = "recommended2")
+    @Mapping(source = "clubBoard.clubBoardId", target = "clubBoardId")
     Recommend recommendPatchDtoToRecommend(RecommendPatchDto recommendPatchDto);
-    @Mapping(source = "recommended", target = "recommended2")
+    @Mapping(source = "clubBoard.clubBoardId", target = "clubBoardId")
     Recommend recommendResponseDtoToRecommend(RecommendResponseDto recommendResponseDto);
     RecommendResponseDto recommendToRecommendResponseDto(Recommend recommend);
 }
