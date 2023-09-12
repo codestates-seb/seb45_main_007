685bde13435efc132de3a8dcb40f2edeb78e64d9
JCI <claeo3@gmail.com>
2023-09-12 16:27:18 +0900

    feat : 빌드 에러 해결

--- Server/main07/src/main/java/com/codestates/main07/clubBoardRecommend/mapper/RecommendMapper.java
+++ Server/main07/src/main/java/com/codestates/main07/clubBoardRecommend/mapper/RecommendMapper.java
@@ -10,11 +10,11 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RecommendMapper {
-    @Mapping(source = "member.nickname", target = "nickname")
+    @Mapping(source = "recommend.recommended", target = "recommended")
     Recommend recommendPostDtoToRecommend(RecommendPostDto recommendPostDto);
-    @Mapping(source = "member.nickname", target = "nickname")
+    @Mapping(source = "recommend.recommended", target = "recommended")
     Recommend recommendPatchDtoToRecommend(RecommendPatchDto recommendPatchDto);
-    @Mapping(source = "member.nickname", target = "nickname")
+    @Mapping(source = "recommend.recommended", target = "recommended")
     Recommend recommendResponseDtoToRecommend(RecommendResponseDto recommendResponseDto);
     RecommendResponseDto recommendToRecommendResponseDto(Recommend recommend);
 }
