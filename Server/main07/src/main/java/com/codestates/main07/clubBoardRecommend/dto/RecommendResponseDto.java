63d61cf25799084b656d4dcf2139f2fa820adce1
JCI <claeo3@gmail.com>
2023-09-12 17:12:23 +0900

    feat : 빌드 에러 해결

--- Server/main07/src/main/java/com/codestates/main07/clubBoardRecommend/dto/RecommendResponseDto.java
+++ Server/main07/src/main/java/com/codestates/main07/clubBoardRecommend/dto/RecommendResponseDto.java
@@ -1,5 +1,7 @@
 package com.codestates.main07.clubBoardRecommend.dto;
 
+import com.codestates.main07.member.entity.Member;
+import com.codestates.main07.clubBoard.board.entity.ClubBoard;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,11 @@ import lombok.NoArgsConstructor;
 @Setter
 @NoArgsConstructor
 public class RecommendResponseDto {
+    private long clubBoardId;
     private boolean recommended;
     private long recommendCount;
+    private Member member;
+    private ClubBoard clubBoard;
 
     public RecommendResponseDto(long recommendCount, boolean recommended) {
         this.recommendCount = recommendCount;
