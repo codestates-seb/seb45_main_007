cfc8bec833b76215028cfda32e80032a7f709675
JCI <claeo3@gmail.com>
2023-09-14 15:13:38 +0900

    feat : likeResponseDto 오탈자 수정.

--- Server/main07/src/main/java/com/codestates/main07/marketBoardLike/dto/LikeResponseDto.java
+++ Server/main07/src/main/java/com/codestates/main07/marketBoardLike/dto/LikeResponseDto.java
@@ -1,4 +1,4 @@
-package com.codestates.main07.maketBoardLike.dto;
+package com.codestates.main07.marketBoardLike.dto;
 
 import com.codestates.main07.member.entity.Member;
 import com.codestates.main07.maketBoard.board.MarketBoard;
