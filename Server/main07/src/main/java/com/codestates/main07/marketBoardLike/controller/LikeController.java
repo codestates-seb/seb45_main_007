5e149ace7ed2d5f97cb332fde223a85c46828902
JCI <91869114+Hyeonbu@users.noreply.github.com>
2023-09-18 09:41:16 +0900

    Update LikeController.java

--- Server/main07/src/main/java/com/codestates/main07/marketBoardLike/controller/LikeController.java
+++ Server/main07/src/main/java/com/codestates/main07/marketBoardLike/controller/LikeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/marketBoard")
+@RequestMapping("/marketBoards")
 public class LikeController {
     private final LikeService likeService;
     private final LikeRepository likeRepository;
